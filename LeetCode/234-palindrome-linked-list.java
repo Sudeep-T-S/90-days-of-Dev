675a3478e9534b024e8eb2a1395654e752e39c69
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-08-17 21:05:42 +0530

    Update 234-palindrome-linked-list.java

--- LeetCode/234-palindrome-linked-list.java
+++ LeetCode/234-palindrome-linked-list.java
@@ -1,3 +1,42 @@
+//3ms of runtime
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null || head.next == null)
+            return true;
+        
+        ListNode fast =  head,
+                 slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+        if (fast != null) 
+            slow = slow.next;
+
+        ListNode prev = null, curr = slow;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        ListNode left = head, right = prev;
+        while (right != null) {
+            if (left.val != right.val)
+                return false;
+            left = left.next;
+            right = right.next;
+        }
+
+        return true;
+    }
+}
+
+//4ms of runtime
 class Solution {
     public boolean isPalindrome(ListNode head) {
 
