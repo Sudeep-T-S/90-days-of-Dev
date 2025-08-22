39b3284d9f75875ac3cb7575f30cce057bb01cfe
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-08-22 14:38:12 +0530

    Create 46-permutations.java

---
+++ LeetCode/46-permutations.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+
+        //Using Backtracking
+        List<List<Integer>> resultList = new ArrayList<>();
+        Stack<List<Integer>> stack = new Stack<>();
+        stack.push(new ArrayList<>());
+
+        while (!stack.isEmpty()) {
+            List<Integer> curr = stack.pop();
+            if (curr.size() == nums.length) {
+                resultList.add(new ArrayList<>(curr));
+            } else {
+                for (int num : nums) {
+                    if (!curr.contains(num)) {
+                        List<Integer> next = new ArrayList<>(curr);
+                        next.add(num);
+                        stack.push(next);
+                    }
+                }
+            }
+        }
+
+        return resultList;
+    }
+}
