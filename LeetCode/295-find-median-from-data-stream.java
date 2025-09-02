a6959b9d61f4da4ccf3780ee04b767ef8b8dc4fc
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-09-02 19:38:11 +0530

    Create 295-find-median-from-data-stream.java

---
+++ LeetCode/295-find-median-from-data-stream.java
@@ -0,0 +1,28 @@
+class MedianFinder {
+
+    private PriorityQueue<Integer> low; 
+    private PriorityQueue<Integer> high; 
+
+    public MedianFinder() {
+
+        low = new PriorityQueue<>(Collections.reverseOrder());
+        high = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+
+        low.offer(num);
+        high.offer(low.poll());
+        
+        if (high.size() > low.size()) 
+            low.offer(high.poll());
+    }
+
+    public double findMedian() {
+
+        if (low.size() > high.size()) 
+            return low.peek();
+        else 
+            return (low.peek() + high.peek()) / 2.0;
+    }
+}
