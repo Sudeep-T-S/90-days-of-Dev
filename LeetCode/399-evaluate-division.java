aceec2bae2fbc9d06cd3a6c0472af485c5437b83
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-09-11 17:58:38 +0530

    Update 399-evaluate-division.java

--- LeetCode/399-evaluate-division.java
+++ LeetCode/399-evaluate-division.java
@@ -1,3 +1,69 @@
+//Using Weighted Union-Find
+class Solution {
+    Map<String, String> parent = new HashMap<>();
+    Map<String, Double> weight = new HashMap<>();
+
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+
+        for (int i = 0; i < equations.size(); i++) {
+            String a = equations.get(i).get(0), b = equations.get(i).get(1);
+            union(a, b, values[i]);
+        }
+
+        double[] res = new double[queries.size()];
+        for (int i = 0; i < queries.size(); i++) {
+
+            String a = queries.get(i).get(0), b = queries.get(i).get(1);
+            if (!parent.containsKey(a) || !parent.containsKey(b)) 
+                res[i] = -1.0;
+            else {
+
+                String rootA = find(a);
+                String rootB = find(b);
+                if (!rootA.equals(rootB)) 
+                    res[i] = -1.0;
+                else 
+                    res[i] = weight.get(a) / weight.get(b);
+            }
+        }
+
+        return res;
+    }
+
+    private String find(String x) {
+
+        if (!parent.get(x).equals(x)) {
+            String origParent = parent.get(x);
+            String root = find(origParent);
+            weight.put(x, weight.get(x) * weight.get(origParent));
+            parent.put(x, root);
+        }
+
+        return parent.get(x);
+    }
+
+    private void union(String a, String b, double value) {
+
+        if (!parent.containsKey(a)) {
+            parent.put(a, a);
+            weight.put(a, 1.0);
+        }
+
+        if (!parent.containsKey(b)) {
+            parent.put(b, b);
+            weight.put(b, 1.0);
+        }
+        
+        String rootA = find(a);
+        String rootB = find(b);
+        if (!rootA.equals(rootB)) {
+            parent.put(rootA, rootB);
+            weight.put(rootA, value * weight.get(b) / weight.get(a));
+        }
+    }
+}
+
+//Using DFS Graph Traversal
 class Solution {
     Map<String, Map<String, Double>> graph = new HashMap<>();
 
