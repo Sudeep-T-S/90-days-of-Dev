939bb7a2fe7d4f1fd54d3bfca1466fc93241c0f4
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-09-11 17:47:56 +0530

    Update 133-clone-graph.java

--- LeetCode/133-clone-graph.java
+++ LeetCode/133-clone-graph.java
@@ -1,3 +1,36 @@
+//Using BFS
+class Solution {
+    public Node cloneGraph(Node node) {
+
+        if (node == null) 
+            return null;
+
+        Map<Node, Node> map = new HashMap<>();
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
+        Node clone = new Node(node.val, new ArrayList<>());
+        map.put(node, clone);
+        
+        while (!queue.isEmpty()) {
+
+            Node curr = queue.poll();
+            for (Node neighbor : curr.neighbors) {
+
+                if (!map.containsKey(neighbor)) {
+
+                    map.put(neighbor, new Node(neighbor.val, new ArrayList<>()));
+                    queue.offer(neighbor);
+                }
+
+                map.get(curr).neighbors.add(map.get(neighbor));
+            }
+        }
+        
+        return clone;
+    }
+}
+
+//Using DFS
 class Solution {
     public Node cloneGraph(Node node) {
         
@@ -13,12 +46,12 @@ class Solution {
         if (map.containsKey(node)) 
             return map.get(node);
 
-        Node copy = new Node(node.val, new ArrayList<>());
-        map.put(node, copy);
+        Node clone = new Node(node.val, new ArrayList<>());
+        map.put(node, clone);
 
         for (Node neighbor : node.neighbors) 
-            copy.neighbors.add(clone(neighbor, map));
+            clone.neighbors.add(clone(neighbor, map));
         
-        return copy;
+        return clone;
     }
 }
