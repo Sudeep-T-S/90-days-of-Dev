f77565ebb3438d63f8c88cd81b583dbd1b5e9be9
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-09-21 21:11:39 +0530

    Update 72-edit-distance.java

--- LeetCode/72-edit-distance.java
+++ LeetCode/72-edit-distance.java
@@ -1,33 +1,29 @@
-//Using Dynamic Progrmming
+//Space-optimized Dynamic Progrmming
 class Solution {
 
     public int minDistance(String word1, String word2) {
 
         int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++) 
-            dp[i][0] = i;
-
+        int[] dp = new int[n + 1];
         for (int j = 0; j <= n; j++) 
-            dp[0][j] = j;
+            dp[j] = j;
 
         for (int i = 1; i <= m; i++) {
 
+            int prev = dp[0];   
+            dp[0] = i;          
             for (int j = 1; j <= n; j++) {
 
+                int temp = dp[j];
                 if (word1.charAt(i - 1) == word2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
+                    dp[j] = prev;
                 else
-                    dp[i][j] = 1 + Math.min(
-                        dp[i - 1][j],      
-                        Math.min(
-                            dp[i][j - 1],  
-                            dp[i - 1][j - 1] 
-                        )
-                    );
+                    dp[j] = 1 + Math.min(Math.min(dp[j], dp[j - 1]), prev);
+                
+                prev = temp;
             }
         }
-
-        return dp[m][n];
+        
+        return dp[n];
     }
 }
