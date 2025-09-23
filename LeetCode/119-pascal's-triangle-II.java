a06988a690870f81c1772a365611307921473fb3
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-09-23 18:34:58 +0530

    Create 119-pascal's-triangle-II.java

---
+++ LeetCode/119-pascal's-triangle-II.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public List<Integer> getRow(int rowIndex) {
+
+        List<Integer> row = new ArrayList<>(Arrays.asList(1));
+        for (int i = 1; i <= rowIndex; i++) {
+
+            row.add(0);
+            for (int j = i; j > 0; j--) 
+                row.set(j, row.get(j) + row.get(j - 1));
+        }
+
+        return row;
+    }
+}
