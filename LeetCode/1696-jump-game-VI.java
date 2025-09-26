536a9f89b849eb229b1e12253b95f174ea9f83cf
Sudeep T S <114288807+Sudeep-T-S@users.noreply.github.com>
2025-09-26 22:07:53 +0530

    Create 1696-jump-game-VI.java

---
+++ LeetCode/1696-jump-game-VI.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int maxResult(int[] nums, int k) {
+
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        Deque<Integer> deque = new LinkedList<>();
+        deque.offer(0);
+
+        for (int i = 1; i < n; i++) {
+            
+            while (!deque.isEmpty() && deque.peekFirst() < i - k) 
+                deque.pollFirst();
+    
+            dp[i] = nums[i] + dp[deque.peekFirst()];
+            while (!deque.isEmpty() && dp[i] >= dp[deque.peekLast()]) 
+                deque.pollLast();
+            
+            deque.offerLast(i);
+        }
+
+        return dp[n - 1];
+    }
+}
